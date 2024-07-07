6c79425dd165f746c2512a3b1e5b8a85358c5248
Liantsoa <liantsoa.rabevahoaka@gmail.com>
2024-07-08 01:58:53 +0300

    create logic we can control who is online + create user resource

--- resources/js/Layouts/ChatLayout.jsx
+++ resources/js/Layouts/ChatLayout.jsx
@@ -1,4 +1,5 @@
 import { usePage } from '@inertiajs/react';
+import { useEffect } from 'react';
 
 const ChatLayout = ({children}) => {
     const page = usePage();
@@ -7,6 +8,25 @@ const ChatLayout = ({children}) => {
 
     console.log("conversations", conversations);
     console.log("selectedConversation", selectedConversation);
+
+    useEffect(() => {
+        Echo.join('online')// with 3 we can control who is online right now 
+            //if i connect to channel, i will get all users here
+            .here((users) => {
+                console.log("here", users)
+            })
+            //joining: ever somebody connect to the channel, that user will come right here
+            .joining((user) => {
+                console.log("joining", user)
+            })
+            //leaving: ever somebody leave that channel, i will get information right here
+            .leaving((user) => {
+                console.log("leaving", user)
+            })
+            .error((error) => {
+                console.log('error', error)
+            })
+    })
 
     return (
         <>
